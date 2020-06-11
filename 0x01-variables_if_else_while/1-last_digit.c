8a29a7f1c50dde4ac733f22fc9d1886461f8b12c
noussair-code <noussair.ouichtati@gmail.com>
2020-06-11 23:22:11 +0100

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
 }
 else if (LastDigit == 0)
 {
-  printf("Last digit of %d is %d and is 0\n", n, LastDigit);
+printf("Last digit of %d is %d and is 0\n", n, LastDigit);
 }
 else
 {
