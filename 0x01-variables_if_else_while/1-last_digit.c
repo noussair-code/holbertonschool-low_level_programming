e166c7fab9c660fa5da9541a74bb37d4203c181d
noussair-code <noussair.ouichtati@gmail.com>
2020-06-11 23:20:14 +0100

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,21 +15,18 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 LastDigit = n % 10;
-{
-printf("Last digit of %d", n);
-printf(" is %d", LastDigit);
-}
+
 if (LastDigit > 5)
 {
-printf(" and is greater than 5 and is not 0\n");
+printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
 }
 else if (LastDigit == 0)
 {
-printf(" and is 0\n");
+  printf("Last digit of %d is %d and is 0\n", n, LastDigit);
 }
 else
 {
-printf(" and is less than 6 and not 0\n");
+printf("Last digit of %d and %d and is less than 6 and not 0\n", n, LastDigit);
 }
 return (0);
 }
