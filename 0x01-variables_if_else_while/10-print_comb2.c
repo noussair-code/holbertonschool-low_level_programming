a09ee30a65e4fb312d112ccd2fc8319da1fefd92
noussair-code <noussair.ouichtati@gmail.com>
2020-06-15 10:27:55 +0100

    print_comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - print_comb2
+* Return: 0
+**/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if ((i == 8) && (j == 9))
+;
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
