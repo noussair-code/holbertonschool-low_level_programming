52f24f9f73d0d62faeae324d9b7334ec9e1ad38b
noussair-code <noussair.ouichtati@gmail.com>
2020-06-16 15:55:40 +0100

    prints_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*print_alphabet_x10 - check the code for Holberton School students.
+*
+*Return: Always 0.
+*/
+void print_alphabet_x10(void);
+{
+int i;
+int b;
+for (i = '1'; i <= 10; i++)
+{
+for (b = 'a'; b <= 10 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
+}
