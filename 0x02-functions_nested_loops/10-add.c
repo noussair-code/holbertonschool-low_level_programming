f94299e94a1c367ab71d8268c922e714f9d01349
noussair-code <noussair.ouichtati@gmail.com>
2020-06-16 17:58:17 +0100

    all

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+*add - adds two integers and returns the result
+*
+* Return: i + j
+*/
+int add(int i, int j)
+{
+return (i + j);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+*print_last_digit - check the code for Holberton School students.
+*i -  variable
+*
+* Return: Always 0.
+*/
+int print_last_digit(int i)
+{
+i = (i % 10);
+if (i < 0)
+i = (i * -1);
+_putchar(i + '0');
+return (i);
+}
