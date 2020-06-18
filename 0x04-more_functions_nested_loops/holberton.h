cd5a3f5891c400420a3c23b70d5c049eb33792e8
noussair-code <noussair.ouichtati@gmail.com>
2020-06-18 22:33:49 +0100

    a

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +11,6 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+
+#endif /* HOLBERTON_H */
