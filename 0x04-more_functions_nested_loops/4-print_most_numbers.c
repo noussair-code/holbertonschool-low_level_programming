e4cbc9bbe514599d19b8553bc8424d4a6805e70e
noussair-code <noussair.ouichtati@gmail.com>
2020-06-18 21:33:17 +0100

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,13 @@
 #include "holberton.h"
+/**
+ * print_most_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void print_most_numbers(void)
 {
 int a = 0;
+
 while (a < 10)
 {
 if ((a != 2) && (a != 4))
