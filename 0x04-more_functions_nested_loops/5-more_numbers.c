adeb7a9f2d7477fdbda2e50973f2c264063e031e
noussair-code <noussair.ouichtati@gmail.com>
2020-06-18 21:36:46 +0100

    update_comment

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,13 @@
 #include "holberton.h"
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void more_numbers(void)
 {
 int i, j;
+
 for (j = 0; j < 10; j++)
 {
 for (i = 0; i <= 14; i++)
