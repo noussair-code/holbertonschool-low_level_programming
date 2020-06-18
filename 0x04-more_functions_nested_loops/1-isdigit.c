c53b422226b1ea35c8acae61e95f4ab14793fc7d
noussair-code <noussair.ouichtati@gmail.com>
2020-06-18 21:17:08 +0100

    is_digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ * _isdigit - check the code for Holberton School students.
+ *@c: int c
+ * Return: Always 0.
+ */
 int _isdigit(int c)
 {
 if ((c >= '0') && (c <= '9'))
